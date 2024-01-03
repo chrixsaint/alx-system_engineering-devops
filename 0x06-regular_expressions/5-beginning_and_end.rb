7cc8349d101175a8329b921d5bf8b73898f4a93b
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-01-03 12:17:00 +0100

    5-beginning_and_end.rb

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if the script is called with an argument
+if ARGV.empty?
+    puts "Usage: #{$0} <string>"
+    exit 1
+  end
+  
+  # Get the first argument passed to the script
+  input_string = ARGV[0]
+  
+  # Define the regular expression to match the pattern
+  regex = /^h.n$/
+  
+  # Use the `match` method of the regular expression to find the match in the input string
+  match_result = input_string.match(regex)
+  
+  # Output the matched string (or an empty string if no match is found)
+  puts match_result ? match_result[0] : ""
+  
